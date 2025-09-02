be36d3e366bf932170f30ef22e24164b6bb89ea4
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-09-02 13:22:22 -0500

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,8 @@
-import { useEffect, useState } from "react"
-import { getAllTickets } from "./services/ticketService"
 import "./App.css"
+import { TicketList } from "./components/tickets/TicketList"
 
 export const App = () => {
-  const [allTickets, setAllTickets] = useState([])
-  const [showEmergencyOnly, setShowEmergencyOnly] = useState(false)
-  const [filteredTickets, setFilteredTickets] = useState([])
-  
-  // on initial load, get all tickets and set that in state
-  useEffect(() => {
-    getAllTickets().then((ticketsArray) => {
-      setAllTickets(ticketsArray)
-      console.log("tickets set!")
-    })
-  }, []) //Only runs on initial render of component
-
-  // when showEmegency or allTickets change and on mount... set filteredTickets to emergency only or all
-  useEffect(() => {
-    if(showEmergencyOnly) {
-      const emergencyTickets = allTickets.filter((ticket) => ticket.emergency === true)
-      setFilteredTickets(emergencyTickets)
-    } else {
-      setFilteredTickets(allTickets)
-    }
-  }, [showEmergencyOnly, allTickets])
-  
-  return <div className="tickets-container">
-    <h2>Tickets</h2>
-    <div>
-      <button 
-        className="filter-btn btn-primary" 
-        onClick={() => {
-          setShowEmergencyOnly(true)
-        }}
-        >Emergency</button>
-      <button 
-        className="filter-btn btn-info" 
-        onClick={() => {
-          setShowEmergencyOnly(false)
-        }}
-        >Show All</button>
-    </div>
-    <article className="tickets">
-      {filteredTickets.map(ticket => {
-        return (
-          <section className="ticket" key={ticket.id}>
-            <header className="ticket-info">#{ticket.id}</header>
-            <div>{ticket.description}</div>
-            <footer>
-              <div>
-                <div className="ticket-info">Emergency</div>
-                <div>{ticket.emergency ? "yes" : "no"}</div>
-              </div>
-            </footer>
-          </section>
-        )
-      })}
-    </article>
-    </div>
-
+  return <>
+    <TicketList />
+  </>
 }
