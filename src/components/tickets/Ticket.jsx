04db6db6f9e4dbe9e2bb060ce34a4c2715493336
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-09-02 13:23:14 -0500

    Added components

---
+++ src/components/tickets/Ticket.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react"
+import { getAllEmployees } from "../../services/employeeService"
+
+export const Ticket = ({ ticket }) => {
+    const [employees, setEmployees] = useState([])
+    const [assignedEmployee, setAssignedEmployee] = useState({})
+
+    useEffect(() => {
+        getAllEmployees().then((employeesArray) => {
+            setEmployees(employeesArray)
+        })
+    }, [])
+
+    useEffect(() => {
+        const foundEmployee = employees.find(
+            (employee) => employee.id === ticket.employeeTickets[0]?.employeeId)
+        setAssignedEmployee(foundEmployee)
+    }, [employees, ticket])
+
+    return (
+        <section className="ticket" >
+            <header className="ticket-info">#{ticket.id}</header>
+            <div>{ticket.description}</div>
+            <footer>
+              <div>
+                <div className="ticket-info">Assignee</div>
+                <div>{assignedEmployee ? assignedEmployee.user?.fullName : "None"}</div>
+              </div>
+              <div>
+                <div className="ticket-info">Emergency</div>
+                <div>{ticket.emergency ? "yes" : "no"}</div>
+              </div>
+            </footer>
+          </section>
+    )
+}
---
+++ src/components/tickets/TicketList.jsx
@@ -0,0 +1,51 @@
+import { useEffect,useState } from "react"
+import { getAllTickets } from "../../services/ticketService"
+import "./Tickets.css"
+import { Ticket } from "./Ticket"
+import { TicketFilterBar } from "./TicketFilterBar"
+
+export const TicketList = () => {
+  const [allTickets, setAllTickets] = useState([])
+  const [showEmergencyOnly, setShowEmergencyOnly] = useState(false)
+  const [filteredTickets, setFilteredTickets] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+  
+  // on initial load, get all tickets and set that in state
+  useEffect(() => {
+    getAllTickets().then((ticketsArray) => {
+      setAllTickets(ticketsArray)
+      console.log("tickets set!")
+    })
+  }, []) //Only runs on initial render of component
+
+  // when showEmegency or allTickets change and on mount... set filteredTickets to emergency only or all
+  useEffect(() => {
+    if(showEmergencyOnly) {
+      const emergencyTickets = allTickets.filter((ticket) => ticket.emergency === true)
+      setFilteredTickets(emergencyTickets)
+    } else {
+      setFilteredTickets(allTickets)
+    }
+  }, [showEmergencyOnly, allTickets])
+
+  useEffect(() => {
+    const foundTickets = allTickets.filter((ticket) => 
+      ticket.description.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    setFilteredTickets(foundTickets)
+  }, [searchTerm, allTickets])
+  
+  return (
+  <div className="tickets-container">
+    <h2>Tickets</h2>
+    <TicketFilterBar setShowEmergencyOnly={setShowEmergencyOnly} 
+    setSearchTerm={setSearchTerm}
+    />
+    <article className="tickets">
+      {filteredTickets.map((ticketObj) => {
+        return <Ticket ticket={ticketObj} key={ticketObj.id}/>
+      })}
+    </article>
+    </div>
+  )
+}
