82ef3acaab4830745d5289815e80e5b1a8c3381e
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-09-03 13:26:16 -0500

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import "./App.css"
 import { CustomerList } from "./components/customers/CustomerList"
 import { EmployeeList } from "./components/employees/EmployeesList"
 import { TicketList } from "./components/tickets/TicketList"
+import { Routes, Route } from "react-router-dom"
 
 export const App = () => {
-  return <>
-    <TicketList />
-    <CustomerList />
-    <EmployeeList />
-  </>
-}
+  return (
+    <Routes>
+      <Route path="/tickets" element={<TicketList />} />
+    </Routes>
+  )
+}     
+//<Route path="/customers" element={<CustomerList />}/>
+//<Route path="/employees" element={<EmployeeList />}/>
